33750a6155ddc58a6b5b78916a502dd2fdaff3b8
lees <leesh7807@gmail.com>
2025-10-14 15:56:43 +0900

    first commit

---
+++ src/main/java/app/leesh/tratic/TraticApplication.java
@@ -0,0 +1,13 @@
+package app.leesh.tratic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TraticApplication.class, args);
+	}
+
+}
---
+++ src/test/java/app/leesh/tratic/TraticApplicationTests.java
@@ -0,0 +1,13 @@
+package app.leesh.tratic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraticApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
